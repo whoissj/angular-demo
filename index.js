12a1e3ff261ef20cd889bd0186fb373e2144dcc9
jun <whoissj@sina.com>
2017-03-22 23:19:07 +0800

    sj

--- index.js
+++ index.js
@@ -3,5 +3,21 @@
  */
 var app = angular.module('app',[]);
 app.controller('firstCtrl',function ($scope) {
-    $scope.typing = 'alert alert-warning'
+    $scope.reversedMes = function (message) {
+        if(message) {
+            return message.split("").reverse().join("");
+        }
+    }
+});
+
+app.controller('secondCtrl',function ($scope,Data) {
+    $scope.data = Data;
+});
+app.controller('thirdCtrl',function ($scope,Data) {
+    $scope.data = Data
+});
+app.factory('Data',function () {
+   return {
+       message:'frome a service'
+   }
 });

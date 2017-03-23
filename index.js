714bec63cbbbdd59c5c9c1fda4fe80bfe4de26e1
whoissj <whoissj@sina.com>
2017-03-23 16:24:55 +0800

    sj

--- index.js
+++ index.js
@@ -21,3 +21,55 @@ app.factory('Data',function () {
        message:'frome a service'
    }
 });
+
+
+app.controller('filterCtrl',function ($scope,Data) {
+      $scope.data = Data
+});
+app.filter('reverse',function(Data){
+  return function(text){
+    if(text) {
+
+    return  '\'' + text.split("").reverse().join("")+ '\'' + "   " + '\'' + Data.message + '\'';
+    }
+  }
+});
+
+app.constant('students', [
+    {name:'夏明',age:13,grade:7,city:"beijing",school:"beida"},
+    {name:'陶渊明',age:9,grade:3,city:"shanghai",school:"qinghua"},
+    {name:'杜甫',age:14,grade:8,city:"shengzhen",school:"fudan"},
+    {name:'王安石',age:11,grade:5,city:"hangzhou",school:"tongji"},
+    {name:'李白',age:7,grade:1,city:"xiamen",school:"keda"},
+    {name:'谢玄',age:15,grade:9,city:'hefei',school:"shangxi"}
+  ]);
+app.constant('labels',[
+    {label:'姓名',name:'name'},
+    {label:'年龄',name:'age'},
+    {label:'年级',name:'grade'},
+    {label:'城市',name:'city'},
+    {label:'学校',name:'school'}
+]);
+app.controller('stnCtrl',function ($scope,students,labels) {
+        $scope.labels = labels;
+        $scope.students = students;
+        $scope.sort = {
+            order:'age',
+            direction:false,
+            icon:'glyphicon-triangle-bottom',
+            orderTurn:function (labelname) {
+                if(labelname === this.order) {
+                    this.direction = !this.direction||false;
+                    if(this.direction) {
+                        this.icon = 'glyphicon-triangle-top';
+                    }else {
+                        this.icon = 'glyphicon-triangle-bottom';
+                    }
+                }else {
+                    this.order = labelname;
+                    this.direction = false;
+                    this.icon = 'glyphicon-triangle-bottom';
+                }
+            }
+        };
+});
